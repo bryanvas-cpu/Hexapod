832faaee90eacfd662d27512caa996f65fcda366
bryanvas-cpu <bryanvas25@gmail.com>
2024-12-01 17:00:33 +0530

    bringup launch file

--- install/setup.ps1
+++ install/setup.ps1
@@ -23,6 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/bryan/bumperbot_ws/install\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/bryan/pico_ws/install\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/bryan/turtlesim_ws/install\local_setup.ps1"
 
---
+++ src/hexapod_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,79 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import GroupAction
+from launch_ros.descriptions import ParameterValue
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("hexapod_description"),
+            "launch",
+            "gazebo.launch.py"
+        )
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("hexapod_controller"),
+            "launch",
+            "controller.launch.py"
+        )
+    )
+    
+    joystick = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("hexapod_controller"),
+            "launch",
+            "joystick_teleop.launch.py"
+        )
+    )
+
+    group = GroupAction(
+        actions=[
+            Node(
+                package="hexapod_controller",
+                executable="inverse_kinematics"
+            ),
+            Node(
+                package="hexapod_controller",
+                executable="body_pose_generator"
+            ),
+            Node(
+                package="hexapod_controller",
+                executable="tip_pose_generator"
+            ),
+            Node(
+                package="hexapod_controller",
+                executable="tip_trajectory_generator"
+            )
+        ]
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(
+                get_package_share_directory("hexapod_description"),
+                "rviz",
+                "hexapod.rviz"
+            )
+        ],
+        output="screen",
+    )
+
+
+    
+    return LaunchDescription([
+        gazebo,
+        controller,
+        joystick,
+        rviz,
+        group
+    ])
